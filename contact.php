068246eed9dfa0365da37009828900e0104a5dd0
noreel <lee.abaca@ciit.edu.ph>
2024-02-29 12:35:14 +0800

    samplecodes

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+
+
+$con = mysqli_connect('localhost', 'root', '','online_store');
+
+// get the post records
+$uname = $_POST['uname'];
+$password = $_POST['password'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$mobile = $_POST['mobile'];
+$address = $_POST['address'];
+$email = $_POST['email'];
+
+
+// database insert SQL code
+$sql = "INSERT INTO `profile` (`username`, `password`, `firstname`, `lastname`, `mobile`, `address`, `email`) VALUES ('$uname', '$password', '$firstname', '$lastname', '$mobile', '$address', '$email')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+
+?>
